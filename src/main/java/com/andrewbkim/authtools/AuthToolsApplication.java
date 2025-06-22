f6da353c1f30ce11c1bab00af8d1b68fbfa590c0
Andrew B. KIM <andrewbkim.dev@gmail.com>
2025-06-22 17:16:09 +0900

    First commit

---
+++ src/main/java/com/andrewbkim/authtools/AuthToolsApplication.java
@@ -0,0 +1,13 @@
+package com.andrewbkim.authtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrewbkim/authtools/AuthToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewbkim.authtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
